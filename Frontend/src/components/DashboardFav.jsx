e741bea82f1942a780ce71fcf05d2c0da2325acc
Anish Dhomase <anishdhomase@gmail.com>
2025-08-05 18:14:45 +0530

    feat(Frontend): Create layout for fav and trash pages

--- Frontend/src/components/DashboardFav.jsx
+++ Frontend/src/components/DashboardFav.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const DashboardFav = () => {
-  return <div>DashboardFav</div>;
+  return (
+    <main className="bg-[#1c2331] w-4/5 p-8 h-screen text-white">
+      Favourites will be implemented here soon ...
+    </main>
+  );
 };
 
 export default DashboardFav;
