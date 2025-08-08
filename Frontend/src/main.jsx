102be5a7a0c7f320b942f275f99a59bb7ec93117
Anish Dhomase <anishdhomase@gmail.com>
2025-08-09 01:37:28 +0530

    security(Frontend): Handle env variable related threat in production

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,11 +5,9 @@ import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import UserContext from "./contexts/UserContext.jsx";
 
-const CLIENT_ID = import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID;
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}>
       <UserContext>
         <App />
       </UserContext>
